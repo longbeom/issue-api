c81fbd76f814b0f1012d20f101f389fba4ccefe3
longbeom <wowow1859@naver.com>
2022-09-23 14:35:12 +0900

    feat: 유저 이름 조회

--- user-service/src/main/kotlin/com/longbeom/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/longbeom/userservice/controller/UserController.kt
@@ -5,6 +5,7 @@ import com.longbeom.userservice.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -33,4 +34,8 @@ class UserController(
     @GetMapping("/me")
     suspend fun get(@AuthToken token: String) : MeResponse =
         MeResponse(userService.getByToken(token))
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(@PathVariable userId: Long) : Map<String, String> =
+        mapOf("reporter" to userService.get(userId).username)
 }
